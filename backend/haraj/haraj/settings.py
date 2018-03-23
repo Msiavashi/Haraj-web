4e0feb6f0728f0ffd090702333d834ea6dfa3930
ms95 <mohammad.siav@hotmail.com>
2018-03-24 03:37:31 +0430

    to merge

--- backend/haraj/haraj/settings.py
+++ backend/haraj/haraj/settings.py
@@ -37,9 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
     'auction'
 ]
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        ],
+
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
