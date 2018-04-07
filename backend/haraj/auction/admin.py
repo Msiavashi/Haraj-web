e09ae9f162a58110f172f6b17c77089b3ee79aba
ms95 <mohammad.siav@hotmail.com>
2018-04-08 02:06:38 +0430

    admin panel

--- backend/haraj/auction/admin.py
+++ backend/haraj/auction/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from auction.models import *
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Payment)
+admin.site.register(Role)
+admin.site.register(Address)
+admin.site.register(Auction)
+admin.site.register(Item)
+admin.site.register(Store)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Comment)
+admin.site.register(Manufacture)
+admin.site.register(Order)
+admin.site.register(Offer)
+admin.site.register(Insurance)
+admin.site.register(Shipment)
